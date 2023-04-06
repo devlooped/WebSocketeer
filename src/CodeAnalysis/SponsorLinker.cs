bce34884915285a87f828818f7569d32454524e0
Daniel Cazzulino <daniel@cazzulino.com>
2023-04-06 00:29:56 -0300

    💟 Add SponsorLink to ensure ongoing maintenance

---
+++ src/CodeAnalysis/SponsorLinker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Devlooped;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace WebSocketeer;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp, LanguageNames.VisualBasic, LanguageNames.FSharp)]
+class SponsorLinker : SponsorLink
+{
+    public SponsorLinker() : base(SponsorLinkSettings.Create(
+        "devlooped", "WebSocketeer",
+        diagnosticsIdPrefix: "WS",
+        version: new Version(ThisAssembly.Info.Version).ToString(3)
+#if DEBUG
+        , quietDays: 0
+#endif
+        ))
+    { }
+}
